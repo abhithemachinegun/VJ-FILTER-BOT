41bbca918807fa1986396d8de9b65d3b5abf9203
Abhishek <117830155+abhithemachinegun@users.noreply.github.com>
2024-07-30 15:39:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'adlinkfly.in')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'd07df00d1bbb8ad905e764a7d3e29a6e8ef6b53f')
